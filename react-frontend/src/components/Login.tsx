c6a65e6b9472f8c3a92318fe58ac36d0b171183c
FIORELLAPS <fiorellapiriz@hotmail.com>
2023-08-01 10:53:28 +0200

    update documentacion

--- react-frontend/src/components/Login.tsx
+++ react-frontend/src/components/Login.tsx
@@ -1,12 +1,27 @@
 // src/components/Login.tsx
 import React, { useState } from "react";
-import { TextField, Button, Paper, Box } from "@mui/material";
+import {
+  TextField,
+  Button,
+  Paper,
+  Box,
+  Snackbar,
+  AlertColor,
+} from "@mui/material";
+import { Alert } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 
 const Login: React.FC = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [showSnackbar, setShowSnackbar] = useState(false);
+  const [snackbarMessage, setSnackbarMessage] = useState("");
+  const [snackbarSeverity, setSnackbarSeverity] = useState<AlertColor>("error"); // 'error', 'success', 'info', or 'warning'
   const navigate = useNavigate();
+
+  const handleSnackbarClose = () => {
+    setShowSnackbar(false);
+  };
 
   const handleLogin = async () => {
     try {
@@ -17,13 +32,14 @@ const Login: React.FC = () => {
         },
         body: JSON.stringify({ username, password }),
       });
-
+      const data = await response.json();
       if (response.ok) {
-        const data = await response.json();
-        console.log(data.message); // Should log "Login successful" on successful login
+        console.log("OK", data.message); // Should log "Login successful" on successful login
         navigate("/dashboard");
       } else {
         console.log("Login failed");
+        setSnackbarMessage("Login failed");
+        setShowSnackbar(true);
       }
     } catch (error) {
       console.error("Error:", error);
@@ -58,6 +74,16 @@ const Login: React.FC = () => {
           Login
         </Button>
       </Box>
+      <Snackbar
+        open={showSnackbar}
+        autoHideDuration={5000}
+        onClose={handleSnackbarClose}
+        anchorOrigin={{ vertical: "top", horizontal: "right" }} // Position the Snackbar at the top center of the screen
+      >
+        <Alert severity={snackbarSeverity} onClose={handleSnackbarClose}>
+          {snackbarMessage}
+        </Alert>
+      </Snackbar>
     </Paper>
   );
 };
