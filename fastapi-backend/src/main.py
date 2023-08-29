208937b3f42f513437f4b705e7b42dc49437c44d
Ana <anasiespe@gmail.com>
2023-08-29 11:32:31 +0200

    Publicación rama ASI y cambios main

--- fastapi-backend/src/main.py
+++ fastapi-backend/src/main.py
@@ -4,6 +4,9 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from pydantic import BaseModel
 
+import pyodbc as pyodbc
+
+import base64
 
 app = FastAPI()
 
@@ -22,12 +25,47 @@ class LoginRequest(BaseModel):
     password: str
 
 @app.post("/login/")
-def login(request: LoginRequest):
+#def login(request: LoginRequest):
     # Replace this with your actual authentication logic
-    print("username", request.username)
-    print("password", request.password)
-    if request.username == "test" and request.password == "password":
-        user = {"username": request.username, "roles": ["admin"], "tags": ["all"]}
-        return user
+    #print("username", request.username)
+    #print("password", request.password)
+    #if request.username == "test" and request.password == "password":
+        #user = {"username": request.username, "roles": ["admin"], "tags": ["all"]}
+        #return user
+    #else:
+        #raise HTTPException(status_code=401, detail="Invalid credentials")
+
+def login(request: LoginRequest):
+
+    # Definir parámetros de conexión
+    server = "192.168.7.128"  # e.g., "localhost" or "your_server_name\instance_name"
+    database = "UserManagement"
+    username = "sa"
+    password = "P@ssw0rd"
+
+    # Definición de string de conexión
+    connection_string = f"DRIVER={{SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password}"
+
+    # Conexión a la base de datos
+    connection = pyodbc.connect(connection_string)
+
+    # Apertura de conexión con SQL
+    cursor = connection.cursor()
+
+    # Si hemos guardado la contraseña codificada
+    pwd_cod = base64.b64encode(request.password.encode("utf-8")).decode("utf-8")
+
+    # 
+    query = "SELECT * FROM Users WHERE Password =  %(pwd_cod)s', (pwd_cod,))"
+    cursor.execute(query)
+
+    # 
+    result = cursor.fetchall()
+    if result[0] == request.username:
+        print("Login successfull")
     else:
-        raise HTTPException(status_code=401, detail="Invalid credentials")
+        print("Usuario o contraseña incorrectos")
+
+    # 
+    cursor.close()
+    connection.close()
