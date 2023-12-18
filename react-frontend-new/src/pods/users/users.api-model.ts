5f8908838668f50ce6904bd5753cb14c2601ac35
Fiorella Piriz Sapio <61502415+Fiorellaps@users.noreply.github.com>
2023-12-18 20:27:03 +0100

    Delete react-frontend-new directory

--- react-frontend-new/src/pods/users/users.api-model.ts
+++
@@ -1,5 +0,0 @@
-export interface MemberUserEntityApi {
-  userName: string;
-  roles: String[];
-  tags: String[];
-}
--- react-frontend-new/src/pods/users/users.api.ts
+++
@@ -1,6 +0,0 @@
-import { MemberUserEntityApi } from "./users.api-model";
-
-export const getUsers = (id: string): Promise<MemberUserEntityApi> =>
-  fetch(`https://api.github.com/users/${id}`).then((response) =>
-    response.json()
-  );
